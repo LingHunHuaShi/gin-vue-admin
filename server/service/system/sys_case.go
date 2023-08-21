9ea3022fc55a7844be2668795144d117b08c898f
jason <monicalace@outlook.com>
2023-08-21 07:36:05 -0400

    building some tables' models.

---
+++ server/service/system/sys_case.go
@@ -0,0 +1,91 @@
+package system
+
+/*
+type SysCase struct {
+	global.GVA_MODEL
+	CaseID     uint      `json:"caseID" gorm:"default:0;primaryKey;autoIncrement;index;comment:案例ID"`
+	UUID       uuid.UUID `json:"uuid" gorm:"comment:案例发起者UUID"`
+	Title      string    `json:"title" gorm:"comment:案例标题"`
+	Content    string    `json:"content" gorm:"comment:案例内容"`
+	SubmitDate time.Time `json:"submitDate" gorm:"comment:提交时间"`
+	Severity   int       `json:"severity" gorm:"default:0;comment:紧急等级"` // 0-3 递增等级
+	Status     int       `json:"status" gorm:"default:0;comment:案例状态"`   // 0未处理 1正在处理 2已完成 3异常
+	DateClosed time.Time `json:"dateClosed" gorm:"comment:完结日期"`
+	Solution   string    `json:"solution" gorm:"comment:解决方案"`
+}
+*/
+
+import (
+	"errors"
+	"github.com/flipped-aurora/gin-vue-admin/server/global"
+	"github.com/flipped-aurora/gin-vue-admin/server/model/system"
+)
+
+type CaseService struct{}
+
+// CreateCase 注册案例创建函数
+// 参数 caseObj 案例对象
+// 返回值 被添加的案例对象 错误信息
+func (c *CaseService) CreateCase(caseObj system.SysCase) (caseInter system.SysCase, err error) {
+	err = global.GVA_DB.Create(&caseObj).Error
+	if err != nil {
+		return caseObj, errors.New("创建案例失败")
+	}
+	return caseObj, err
+}
+
+// FindCaseByUUID 根据发起者UUID查找案例
+// 参数 uuid 发起者UUID
+// 返回值 查找到的案例对象指针 错误信息
+func (c *CaseService) FindCaseByUser(user system.SysUser) (caseInter *system.SysCase, err error) {
+	var caseObj system.SysCase
+	err = global.GVA_DB.Where("UUID = ?", user.UUID).First(&caseObj).Error
+	if err != nil {
+		return &caseObj, errors.New("案例不存在")
+	}
+	return &caseObj, err
+}
+
+// FindCaseByCaseID 根据案例ID查找案例
+// 参数 caseID 案例ID
+// 返回值 查找到的案例对象指针 错误信息
+func (c *CaseService) FindCaseByCaseID(caseID uint) (caseInter *system.SysCase, err error) {
+	var caseObj system.SysCase
+	err = global.GVA_DB.Where("CaseID = ?", caseID).First(&caseObj).Error
+	if err != nil {
+		return &caseObj, errors.New("案例不存在")
+	}
+	return &caseObj, err
+}
+
+// DeleteCase 根据案例ID删除案例
+// 参数 caseID 案例ID
+// 返回值 错误信息
+func (c *CaseService) DeleteCase(caseID uint) (err error) {
+	var caseObj system.SysCase
+	err = global.GVA_DB.Where("CaseID = ?", caseID).Delete(&caseObj).Error
+	if err != nil {
+		return errors.New("案例不存在")
+	}
+	return err
+}
+
+// UpdateCaseByCaseID 根据案例ID更新案例信息
+// 参数 caseID 案例ID, newCase 新的案例信息
+// 返回值 更新后的案例对象指针 错误信息
+func (c *CaseService) UpdateCaseByCaseID(caseID uint, newCase system.SysCase) (updateCase *system.SysCase, err error) {
+	var caseObj system.SysCase
+
+	// 查询案例是否存在
+	err = global.GVA_DB.Where("CaseID = ?", caseID).First(&caseObj).Error
+	if err != nil {
+		return &caseObj, errors.New("案例不存在")
+	}
+
+	// 更新案例信息
+	err = global.GVA_DB.Model(&caseObj).Updates(newCase).Error
+	if err != nil {
+		return &caseObj, errors.New("更新失败")
+	}
+	return &caseObj, err
+}
---
+++ server/service/system/sys_task.go
@@ -0,0 +1,92 @@
+package system
+
+import (
+	"errors"
+	"github.com/flipped-aurora/gin-vue-admin/server/global"
+	"github.com/flipped-aurora/gin-vue-admin/server/model/system"
+)
+
+/*
+type SysTask struct {
+	global.GVA_MODEL
+	TaskID       uint      `json:"taskID" gorm:"default:0;primaryKey;autoIncrement;index;comment:任务ID"` // 任务ID
+	UUID         uuid.UUID `json:"uuid" gorm:"comment:用户UUID"`                                          // 用户UUID
+	VideoSource  string    `json:"videoSource" gorm:"comment:视频源"`                                      // 视频源
+	CreationDate time.Time `json:"creationDate" gorm:"comment:创建时间"`
+	Resolution   int       `json:"resolution" gorm:"comment:分辨率等级"`
+	AlgorithmID  uint      `json:"algorithmId" gorm:"comment:任务容器算法ID"`
+	Intensity    int       `json:"intensity" gorm:"comment:任务粒度"`
+	Status       int       `json:"status" gorm:"comment:任务状态"`
+}
+*/
+
+type TaskService struct{}
+
+// CreateTask 注册任务创建函数
+// 参数 task 任务对象
+// 返回值 被添加的任务对象 错误信息
+func (t *TaskService) CreateTask(task system.SysTask) (taskInter system.SysTask, err error) {
+	err = global.GVA_DB.Create(&task).Error
+	if err != nil {
+		return task, errors.New("创建任务失败")
+	}
+	return task, err
+}
+
+// FindTaskByUser 根据用户查找任务
+// 参数 user 系统用户对象
+// 返回值 查找到的任务对象指针 错误信息
+func (t *TaskService) FindTaskByUser(user system.SysUser) (taskInter *system.SysTask, err error) {
+	var task system.SysTask
+	uuid := user.UUID
+	// 查找要更新的算法
+	err = global.GVA_DB.Where("UUID = ?", uuid).First(&task).Error
+	if err != nil {
+		return &task, errors.New("任务不存在")
+	}
+	return &task, err
+}
+
+// FindTaskByTaskID 根据任务ID查找任务
+// 参数 taskID 任务ID
+// 返回值 查找到的任务对象指针 错误信息
+func (t *TaskService) FindTaskByTaskID(taskID uint) (taskInter *system.SysTask, err error) {
+	var task system.SysTask
+	err = global.GVA_DB.Where("TaskID = ?", taskID).First(&task).Error
+	if err != nil {
+		return &task, errors.New("任务不存在")
+	}
+	return &task, err
+}
+
+// DeleteTask 根据任务ID删除任务
+// 参数 taskID 任务ID
+// 返回值 错误信息
+func (t *TaskService) DeleteTask(taskID uint) (err error) {
+	var task system.SysTask
+	err = global.GVA_DB.Where("TaskID = ?", taskID).Delete(&task).Error
+	if err != nil {
+		return errors.New("任务不存在")
+	}
+	return err
+}
+
+// UpdateTaskByTaskID 根据任务ID更新任务信息
+// 参数 taskID 任务ID, newTask 新的任务信息
+// 返回值 更新后的任务对象指针 错误信息
+func (t *TaskService) UpdateTaskByTaskID(taskID uint, newTask system.SysTask) (updateTask *system.SysTask, err error) {
+	var task system.SysTask
+
+	// 查询任务是否存在
+	err = global.GVA_DB.Where("TaskID = ?", taskID).First(&task).Error
+	if err != nil {
+		return &task, errors.New("任务不存在")
+	}
+
+	// 更新任务信息
+	err = global.GVA_DB.Model(&task).Updates(newTask).Error
+	if err != nil {
+		return &task, errors.New("更新失败")
+	}
+	return &newTask, err
+}
