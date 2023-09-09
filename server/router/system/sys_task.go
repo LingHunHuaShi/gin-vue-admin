a869e55b1553a6471c529f309ad2b0769326dee3
jason <monicalace@outlook.com>
2023-09-08 22:49:49 -0400

    finished the api of tasks.

--- server/router/system/sys_task.go
+++ server/router/system/sys_task.go
@@ -1,10 +1,21 @@
 package system
 
+import (
+	v1 "github.com/flipped-aurora/gin-vue-admin/server/api/v1"
+	"github.com/flipped-aurora/gin-vue-admin/server/middleware"
+	"github.com/gin-gonic/gin"
+)
+
 type TaskRouter struct{}
 
-//func InitTaskRouter(Router *gin.RouterGroup) {
-//	TaskRouter := Router.Group("task").Use(middleware.OperationRecord())
-//	{
-//
-//	}
-//}
+func InitTaskRouter(Router *gin.RouterGroup) {
+	TaskRouter := Router.Group("task").Use(middleware.OperationRecord())
+	TaskApi := v1.ApiGroupApp.SystemApiGroup.TaskApi
+	{
+		TaskRouter.POST("CreateTask", TaskApi.CreateTask)
+		TaskRouter.GET("FindTaskByTaskID", TaskApi.FindTaskByTaskID)
+		TaskRouter.GET("FindTaskByUser", TaskApi.FindTaskByUser)
+		TaskRouter.GET("UpdateTask", TaskApi.UpdateTask)
+		TaskRouter.DELETE("DeleteTask", TaskApi.DeleteTask)
+	}
+}
