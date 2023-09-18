ede2f11a4d701b8ac2b759d3785e98bb65520962
jason <monicalace@outlook.com>
2023-09-17 22:39:40 -0400

    上传数据到云端的后端实验API代码补充

--- server/api/v1/system/sys_status.go
+++ server/api/v1/system/sys_status.go
@@ -22,3 +22,43 @@ func (Api *SysStatusApi) UploadSystemStatus(c *gin.Context) {
 	}
 	response.OkWithMessage("系统数据成功上传", c)
 }
+
+// API提交数据给云端
+/*
+func (Api *SysStatusApi) SendSystemStatusViaAPI(c *gin.Context) {
+	var Status system.SysStatus
+	err := c.ShouldBindJSON(&Status)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// API请求
+	ApiUrl := "https://..."
+	status, err := json.Marshal(Status)
+	if err != nil {
+		response.FailWithMessage(err.Error(), c)
+		return
+	}
+	res, err := http.Post(ApiUrl, "application/json", bytes.NewBuffer(status))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error sending request"})
+		return
+	}
+	defer res.Body.Close()
+
+	// 处理响应
+	if res.StatusCode == http.StatusOK {
+		// 读取和处理响应数据，可能需要使用JSON解析
+		responseData, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error reading response"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"response": string(responseData)})
+	} else {
+		c.JSON(res.StatusCode, gin.H{"error": "Request failed"})
+	}
+}
+*/
